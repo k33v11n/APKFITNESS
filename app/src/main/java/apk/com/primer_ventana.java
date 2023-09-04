9e9c485db8859c5955346eba67cb7f40843aef9c
unknown <sistemas@CHI051PCAL019.alumnos.cst>
2023-09-04 16:16:17 -0300

    Initial commit

---
+++ app/src/main/java/apk/com/primer_ventana.java
@@ -0,0 +1,14 @@
+package apk.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class primer_ventana extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primer_ventana);
+    }
+}
